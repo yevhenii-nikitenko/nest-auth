f30104d83505a9c33c366a6530361102eebe692a
Yevhenii Nikitenko <nikitenko@Yevheniis-MacBook-Pro.local>
2023-12-07 18:20:03 +0200

    change structure

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
 import { ConfigModule } from '@nestjs/config';
+
+import { AuthService } from './service/auth.service';
+import { AuthController } from './controller/auth.controller';
 
 @Module({
     imports: [ConfigModule],
